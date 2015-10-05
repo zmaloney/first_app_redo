66eda28ffa1314fd5ea704da5ff9c3231e7fe011
Zacharie Maloney <zacharie.maloney@gmail.com>
2015-10-05 17:16:59 -0400

    Test to refresh ignore

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ActiveRecord::Base        
-  attr_accessible :content, :user_id
-  validates :content, :length => { :maximum => 140 }
+  # attr_accessible :content, :user_id                
+  belongs_to :user #a post belongs to a user
+  validates :content, :length => { :maximum => 140 } # and shouldn't have more than 140 characters. 
 end
