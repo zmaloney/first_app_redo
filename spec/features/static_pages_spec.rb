e4cef0ef2a98344195eb2d0c73cd54a93348a49e
Zacharie Maloney <zacharie.maloney@gmail.com>
2015-10-18 20:09:58 -0400

    Got the layout page working correctly to provide standardized titles for all static_pages documents.

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -3,27 +3,47 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home page : GET /static_pages" do
 
-    it "should have the content 'Sample App'" do 
+    it "should have the h1 value 'Sample App'" do 
       visit '/static_pages/home'
-      page.should have_content('Sample App')
-     end
+      page.should have_selector('h1', :text => 'Sample App')
+     end     
+     
+     it "should have the title 'Home'" do 
+       visit '/static_pages/home'
+       page.should have_selector('title', 
+        :text => "Sample App | Home") 
+      end
 
   end      
   
   describe "Help page : GET /static_pages" do
 
-    it "should have the content 'How to get help'" do 
+    it "should have the h1 value 'Help'" do 
       visit '/static_pages/help'
-      page.should have_content('How to get help')
-     end
+      page.should have_selector('h1', :text => 'Help')
+     end   
+     
+     it "should have the title 'Help'" do 
+       visit '/static_pages/help'
+       page.should have_selector('title', 
+        :text => "Sample App | Help")
+      end
 
   end   
   
-  describe "About page : GET /static_pages" do 
-    it "should have the content 'About'" do 
+  describe "About page : GET /static_pages" do    
+    
+    it "should have the h1 value 'About'" do 
       visit '/static_pages/about'
-      page.should have_content('About this site')
-    end
+      page.should have_selector('h1', :text => 'About')
+    end 
+    
+    it "should have the title 'About'" do 
+      visit '/static_pages/about'
+      page.should have_selector('title', 
+        :text => "Sample App | About")
+     end
+      
   end
   
 end
